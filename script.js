b900861de20f3f8c71a2f818fcd743c27f67a732
gonzalez-7 <20agonzalezp@gmail.com>
2024-11-06 20:15:30 -0800

    Output the first and last values of the array

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Step 1: Instantiate a new array and add 5 numeric values to it
 let myArray = [10, 20, 30, 40, 50];
 
+// Step 2: Output the first and last values of the array
+console.log('First value:', myArray[0]);
+console.log('Last value:', myArray[myArray.length - 1]);
 
